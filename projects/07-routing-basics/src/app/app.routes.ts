22e4832dd24ac4c7f63c8de844e31268de35b40c
Jeyhun Abilov <abilovv599@gmail.com>
2024-02-22 18:53:38 +0400

    learn routing

--- projects/07-routing-basics/src/app/app.routes.ts
+++ projects/07-routing-basics/src/app/app.routes.ts
@@ -1,7 +1,16 @@
 import { Routes } from '@angular/router';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { AppComponent } from './app.component';
+import { GreetingsComponent } from './greetings.component';
 
 export const routes: Routes = [
   {
+    path: '',
+    component: GreetingsComponent,
+  },
+  {
     // add a default path to greetings component
+    path: 'about',
+    component: AboutPageComponent,
   },
 ];
